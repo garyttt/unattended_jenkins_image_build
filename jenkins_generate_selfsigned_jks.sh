065bee5b76bbdf6006ecc921a224f3632771bf77
Gary Tay <garyttt@singnet.com.sg>
2020-07-20 07:02:05 +0800

    generate jks script

--- jenkins_generate_selfsigned_jks.sh
+++ jenkins_generate_selfsigned_jks.sh
@@ -1,6 +1,3 @@
-generate_self_signed_jks () 
-{ 
     keytool -genkey -keyalg RSA -alias selfsigned -keystore selfsigned.jks -validity 365 -keysize 4096;
     keytool -export -alias selfsigned -keystore selfsigned.jks -rfc -file selfsigned.cer;
     keytool -list -v -keystore selfsigned.jks
-}
