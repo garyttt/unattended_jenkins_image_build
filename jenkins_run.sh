ca3c2e8109bfa8337b0ba82e2e1ee8a55cbd2b08
Gary Tay <garyttt@singnet.com.sg>
2020-07-29 21:03:19 +0800

    updated run script

--- jenkins_run.sh
+++ jenkins_run.sh
@@ -17,14 +17,12 @@ docker run \
 --detach \
 --publish 8080:8080 \
 --publish 50000:50000 \
---publish 8083:8083 \
---publish 50022:50022 \
 --mount type=bind,src=$PWD/jenkins_home,dst=/var/jenkins_home \
 --mount type=bind,src=$PWD/jenkins_config_backup,dst=/var/tmp/jenkins_config_backup \
 --env JAVA_OPTS="-Djenkins.install.runSetupWizard=false -Djava.util.logging.config.file=/var/jenkins_home/logging.properties" \
---env JENKINS_OPTS="--prefix=/jenkins --httpPort=-1 --httpsPort=8083 --httpsKeyStore=/var/jenkins_home/selfsigned.jks --httpsKeyStorePassword=secret" \
+--env JENKINS_OPTS="--prefix=/jenkins" \
 --env TZ=Asia/Singapore \
---health-cmd "curl --insecure --head http://localhost:8080 && exit 0 || exit 1" \
+--health-cmd "curl --insecure --head http://localhost:8080/jenkins && exit 0 || exit 1" \
 --health-interval 5s \
 --health-timeout 3s \
 --health-retries 3 \
@@ -33,11 +31,7 @@ $REPO/$IMAGE:$TAG
 
 docker logs -f $IMAGE
 
-# Revert back to http, copy the below to replace the above
-#--env JENKINS_OPTS="--prefix=/jenkins --httpPort=8080 httpsPort=-1" \
-
-# For sone reason httpsCertificate and httpsPrivateLey do not work
-# Sp please replace
-#--env JENKINS_OPTS="--prefix=/jenkins --httpPort=-1 --httpsPort=8083 --httpsCertificate=/var/jenkins_home/server.crt --httpsPrivateKey=/var/jenkins_home/server.key" \
-# with
+# https
+#--publish 8083:8083 \
 #--env JENKINS_OPTS="--prefix=/jenkins --httpPort=-1 --httpsPort=8083 --httpsKeyStore=/var/jenkins_home/selfsigned.jks --httpsKeyStorePassword=secret" \
+#--health-cmd "curl --insecure --head https://localhost:8083/jenkins && exit 0 || exit 1" \
